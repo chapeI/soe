af0d7303bdd7ef0e7f890f60ac5a8a5fe1b1de83
chapel <anoopanthony@gmail.com>
2022-08-11 14:16:43 -0400

    refactoring setItems()
    +setCalendarAndRequirements()
    +checkIfNullCalendarOrRequirement
    +setDefaultCalendarRequirements
    +loadLocallySavedCalendarAndRequirements

--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -9,37 +9,69 @@ import { LocalService } from '../local.service';
 })
 export class SchedulerComponent implements OnInit {
 
+  requirements: string[] = []
+  calendar: string[] = []
+
   constructor(private localService: LocalService) { }
 
-  ngOnInit(): void {
-    if(this.localService.getData('calendar') != null) {
-      let calendar_parsed = JSON.parse(this.localService.getData('calendar'))
-      this.calendar = calendar_parsed
-    }
-  }
-
-  calendar = [
-    'move into this calendar'
-  ];
-
-  requirements = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep',
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
+  ngOnInit() { this.setCalendarAndRequirements() }
 
   drop(event: CdkDragDrop<string[]>) {
     transferArrayItem(event.previousContainer.data,
                       event.container.data,
                       event.previousIndex,
                       event.currentIndex);
+
     let calendar_stringifyd = JSON.stringify(this.calendar)
     this.localService.saveData('calendar', calendar_stringifyd)    
+
+    let requirements_stringifyd = JSON.stringify(this.requirements)
+    this.localService.saveData('requirements', requirements_stringifyd)
   }
+
+  setCalendarAndRequirements() {
+    if(this.checkIfNullCalendarOrRequirements()) {
+      this.setDefaultCalendarAndRequirements()
+    } else {
+      this.loadLocallySavedCalendarAndRequirements()
+    }
+  }
+
+  checkIfNullCalendarOrRequirements(): boolean {
+    return (this.localService.getData('calendar') == null || this.localService.getData('requirements') == null)
+  }
+
+  setDefaultCalendarAndRequirements() {
+    this.calendar = [
+      'move into this calendar'
+    ];
+
+    this.requirements = [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep',
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ];
+  } 
+  
+  loadLocallySavedCalendarAndRequirements() {
+    this.setLocalCalendar()
+    this.setLocalRequirements()
+  }
+
+  setLocalCalendar(){
+    let calendar_parsed = JSON.parse(this.localService.getData('calendar'))
+    this.calendar = calendar_parsed
+  }
+
+  setLocalRequirements(){
+    let requirements_parsed = JSON.parse(this.localService.getData('requirements'))
+    this.requirements = requirements_parsed
+  }
+
 }
