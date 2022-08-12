212350aa7ff01805e0fe231873d840f675cc4bdc
chapel <anoopanthony@gmail.com>
2022-08-12 19:56:00 -0400

    +expansion panels
    requirements placed into expandable sections
    progress-bar placed into panel description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { LocalService } from './local.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     DragDropModule,
     MatProgressSpinnerModule,
     MatProgressBarModule,
+    MatExpansionModule,
   ],
   providers: [
     LocalService
