0c959da34161dc7f1545db5cdbd2d15e24d941a5
chapel <anoopanthony@gmail.com>
2022-08-13 12:10:21 -0400

    +mat-fab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { DevComponent } from './dev/dev.component';
-
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -37,6 +37,7 @@ import { DevComponent } from './dev/dev.component';
     MatProgressBarModule,
     MatExpansionModule,
     MatButtonModule,
+    MatIconModule,
   ],
   providers: [
     LocalService
