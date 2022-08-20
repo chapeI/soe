9223b8989eb47439b73e1388ac452a2e0cbdca5d
chapel <anoopanthony@gmail.com>
2022-08-20 19:04:30 -0400

    + flow - scheduler(html,ts)
    only keeping logical flow

--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -1,14 +1,7 @@
 import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { filter, map, Observable } from 'rxjs';
 import { FirebaseyService } from '../firebasey.service';
 import { LocalService } from '../local.service';
-
-export interface Course {
-  name: string
-  color: string
-}
 
 @Component({
   selector: 'app-scheduler',
@@ -17,128 +10,64 @@ export interface Course {
 })
 export class SchedulerComponent implements OnInit {
 
-  core: Course[] = []
-  cs: Course[] = []
+  calendar = []
+  requirements = []
 
-  terms = [
-    {'courses': [{'name': 'add terms, drag courses into terms', 'color': 'grey'}]},
-  ]
-
-  constructor(private localService: LocalService, public dialog: MatDialog, private firebaseService: FirebaseyService) { }
+  constructor(private localService: LocalService, private firebaseService: FirebaseyService) { }
 
   ngOnInit() {
     this.setup()
   }
 
-  addTerm() {
-    this.terms.push(
-      {'courses': []},
-    )
-    this.saveState()
-   }
-
-  removeTerm() {
-    this.terms.pop()
-    this.persistUsersTerms()
-  }
-
-  persistUsersTerms() {
-    let stringed = JSON.stringify(this.terms)
-    this.localService.save('terms', stringed)
-  }
-
   setup() {
-    if(this.noLocallySavedData()) {
-      this.setDefaults()
+    if(this.noLocalData()) {
+      this.findRequirements()
     } else {
-      this.loadLocallySavedData()
+      this.loadLocalData()
     }
   }
 
-  loadLocallySavedData() {
-    this.loadTermsAddedByUser()
-    this.loadLocalRequirements()
+  loadLocalData() {
+    console.log('setting local data');
+    this.grabLocalCalendar()
+    this.grabLocalRequirements()
   }
 
-  loadTermsAddedByUser(){
-    let parsed = JSON.parse(this.localService.get('terms'))
-    this.terms = parsed
-  }
-
-  noLocallySavedData(): boolean {
-    return (this.localService.get('terms') == null)
-  }
-
-  drop(event: CdkDragDrop<Course[]>) {
-    transferArrayItem(event.previousContainer.data,
-                      event.container.data,
-                      event.previousIndex,
-                      event.currentIndex);
-    this.saveState()
-  }
-
-  saveState() {
-    this.persistUsersTerms()
-    this.persistRequirements()
-  }
-
-  persistRequirements() {
-    this.locallySave('core', this.core)
-    this.locallySave('cs', this.cs)
-  }
-
-  locallySave(name: string, list: Course[]) {
-    let stringifyd = JSON.stringify(list)
-    this.localService.save(name, stringifyd)
-  }
-
-  setDefaults() {
-    this.setASingleTerm()
-    this.setRequirements()
-  }
-
-  setASingleTerm() {
-    // variable definition does this by default
-  }
-
-  setRequirements() {
-    console.log('entered defaults');
-    // loadFromDatabase()
-
-    this.core = [
-      {'name': 'data structures and algorithms', 'color': 'red'},
-      {'name': 'system hardware',                'color': 'red'},
-    ];
-
-    this.cs = [
-      {'name': 'web development',  'color': 'blue'},
-      {'name': 'information security',  'color': 'blue'},
-    ]
-
-  }
-
-  resetCalendar() {
-    this.localService.clear()
-    this.refreshPage()
-  }
-
-  refreshPage() {
-    window.location.reload()
-  }
-
-  loadLocalRequirements() {
+  grabLocalRequirements() {
     this.loadCores()
-    this.loadElectives()
   }
 
   loadCores(){
     let parsed = JSON.parse(this.localService.get('core'))
-    this.core = parsed
   }
 
-  loadElectives(){
-    let parsed = JSON.parse(this.localService.get('cs'))
-    this.cs = parsed
+  grabLocalCalendar(){
+    let parsed = JSON.parse(this.localService.get('calendar'))
+  }
+
+  noLocalData(): boolean {
+    return (this.localService.get('calendar') == null)
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    transferArrayItem(event.previousContainer.data,
+                      event.container.data,
+                      event.previousIndex,
+                      event.currentIndex);
+    this.persistCalendar()
+    this.persistRequirements()
+  }
+
+  persistRequirements() { }
+  persistCalendar() {}
+
+  persist(name: string, list: string[]) {
+    let stringifyd = JSON.stringify(list)
+    this.localService.save(name, stringifyd)
+  }
+
+  findRequirements() {
+    console.log('FIND REQUIREMENTS');
   }
 
 }
