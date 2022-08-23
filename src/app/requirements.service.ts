318de104979bc06b01bf226388aa8bf3ba6b21c4
chapel <anoopanthony@gmail.com>
2022-08-23 00:24:34 -0400

    - requirementsService.everything

--- src/app/requirements.service.ts
+++ src/app/requirements.service.ts
@@ -1,41 +1,10 @@
 import { Injectable } from '@angular/core';
 
-class Course {
-  name
-  requirement
-  constructor(name: string, category:string) {
-    this.name = name
-    this.requirement = category
-  }
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequirementsService {
 
-  courses: Course[] = [
-    {'name': 'data structures', 'requirement': 'core'},
-    {'name': 'operating systems', 'requirement': 'core'},
-    {'name': 'calculus', 'requirement': 'math'},
-    {'name': 'ethics', 'requirement': 'english' },
-    {'name': 'anthropology', 'requirement': 'general' },
-    {'name': 'philosophy', 'requirement': 'general' },
-  ]
-
-  constructor() { }
-
-  getRequirements() {
-    return this.courses;
-  }
-
-  getSortedCourses() {
-    let o: any = {}
-    this.courses.map(c => {
-      o[c.requirement] = o[c.requirement] || []     // undefined ? [] : [lstSoFar]   // value undefined only on seeing a category for the first time
-      o[c.requirement] = [...o[c.requirement], c]   // unwrap [], append course
-    })
-    return o
-  }
 
 }
