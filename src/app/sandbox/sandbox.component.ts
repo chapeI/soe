83d44f02fd9eb7c1409e4621ad2184fee8b7eceb
chapel <anoopanthony@gmail.com>
2022-09-01 12:55:32 -0400

    + cdk.dragNdrop

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,6 +11,23 @@ export class SandboxComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
